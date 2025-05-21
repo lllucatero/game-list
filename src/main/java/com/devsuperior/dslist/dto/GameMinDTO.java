56b716e5f747937c0554cc586798dbe74f8339bd
Lucas Pontes <lcspontes@gmail.com>
2025-05-20 22:36:41 -0300

    Create Game Service, Repository, DTO and Controllers

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+
+	public GameMinDTO() {
+		
+	}
+
+	public GameMinDTO(Game Entity) {
+		id = Entity.getId();
+		title = Entity.getTitle();
+		year = Entity.getYear();
+		imgUrl = Entity.getImgUrl();
+		shortDescription = Entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+}
